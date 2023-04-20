619d3d71a4d96db1d1b94c49f2d19f6ecab70c3b
Rebecca Masenya <rebs_2015@outlook.com>
2023-04-20 15:12:16 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <ststd.h>
+#include <stdio.h>
 /**
  * main - Entry point
- * Return: 0 (Success)
+ *Return: 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0)
-;
+return (0);
+}
