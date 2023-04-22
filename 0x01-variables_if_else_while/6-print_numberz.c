4a126f8450d897a828137b6b68c788a9404fc461
Rebecca Masenya <rebs_2015@outlook.com>
2023-04-21 18:29:34 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,25 +5,11 @@
  */
 int main(void)
 {
-putchar('0');
-putchar('\n');
-putchar('1');
-putchar('\n');
-putchar('2');
-putchar('\n');
-putchar('3');
-putchar('\n');
-putchar('4');
-putchar('\n');
-putchar('5');
-putchar('\n');
-putchar('6');
-putchar('\n');
-putchar('7');
-putchar('\n');
-putchar('8');
-putchar('\n');
-putchar('9');
+int num;
+for (num = 0; num < 10; num++)
+{
+putchar ((num % 10) + '0');
+}
 putchar('\n');
 return (0);
 }
