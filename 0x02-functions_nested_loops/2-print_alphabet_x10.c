da3fcb8bf0549a264f98e9e06ffea2d178955376
Rebecca Masenya <rebs_2015@outlook.com>
2023-04-26 04:05:29 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_alphabet_x10 - Make alphabet ×10 times
  * Return: void
@@ -14,6 +13,7 @@ for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
-_putchar('\n'); i++;
+_putchar('\n');
+i++;
 }
 }
