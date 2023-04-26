76f550de94528102fe7a44a83d2838df7abd96fa
Rebecca Masenya <rebs_2015@outlook.com>
2023-04-26 07:59:43 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("+%d\n", n);
+_putchar(43);
 return (1);
 }
 else if (n < 0)
 {
-printf("%d\n", n);
+_putchar(45);
 return (-1);
 }
 else
 {
-printf("0\n");
+_putchar(48);
 return (0);
 }
 }
