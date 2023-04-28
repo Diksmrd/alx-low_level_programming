c6fb2463d82f108c72857cd2450be7a5b08d3ab6
Rebecca Masenya <rebs_2015@outlook.com>
2023-04-27 21:33:14 -0700

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - Determine if a random number is positive, negative or zero.
+ *Return: 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+if (i > 0)
+printf("%d is positive\n", i);
+else if (i < 0)
+printf("%d is negative\n", i);
+else
+printf("%d is zero\n", i);
+}
