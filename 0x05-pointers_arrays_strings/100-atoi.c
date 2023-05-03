a41ff8e609c41cd90008430ac31d9f32818360d6
Rebecca Masenya <rebs_2015@outlook.com>
2023-05-03 12:12:07 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to convert
+ *
+ * Return: the integer value of the string, or 0 if there are no numbers
+ */
+
+int _atoi(char *s)
+{
+int result = 0;
+int sign = 1;
+int i = 0;
+
+if (s[i] == '-')
+{
+sign = -1;
+i++;
+}
+else if (s[i] == '+')
+{
+i++;
+}
+
+while (s[i] != '\0')
+{
+if (s[i] >= '0' && s[i] <= '9')
+{
+result = result * 10 + (s[i] - '0');
+i++;
+}
+else
+{
+break;
+}
+}
+return (result *sign);
+}
