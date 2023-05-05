1e8d0a938b6a12722b9f9543e6a40c7dae64573f
Rebecca Masenya <rebs_2015@outlook.com>
2023-05-04 19:54:31 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,31 +1,27 @@
 #include "main.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * leet - Encodes a string into 1337.
- * @s: The input string.
+ * leet - encodes a string into 1337.
+ * @n: The input string.
  *
- * Return: The encoded string.
+ * Return: value of n
  */
-char *leet(char *s)
+
+char *leet(char *n)
 {
-char *result = malloc(strlen(s) + 1);
 int i, j;
-char *leet_table[256] = {0};
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
 
-leet_table['a'] = leet_table['A'] = "4";
-leet_table['e'] = leet_table['E'] = "3";
-leet_table['o'] = leet_table['O'] = "0";
-leet_table['t'] = leet_table['T'] = "7";
-leet_table['l'] = leet_table['L'] = "1";
-
-for (i = 0, j = 0; s[i]; i++, j++)
+for (i = 0; n[i] != '\0'; i++)
 {
-char *replacement = leet_table[(unsigned char) s[i]];
-result[j] = replacement ? *replacement : s[i];
+for (j = 0; j < 10; j++)
+{
+if (n[i] == s1[j])
+{
+n[i] = s2[i];
 }
-result[j] = '\0';
-
-return (result);
+}
+}
+return (n);
 }
