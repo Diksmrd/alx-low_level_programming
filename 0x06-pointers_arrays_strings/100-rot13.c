df96fd80a1a20ef7f4e95fe340307f3ddf375d95
Rebecca Masenya <rebs_2015@outlook.com>
2023-05-05 19:56:31 -0700

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * rot13 - encode a string using rot13
+ * @str: The input string.
+ *
+ * Return: The encoded string.
+ */
+
+char *rot13(char *str)
+{
+char *result = (char *) malloc(sizeof(char) * (strlen(str) + 1));
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'A' && str[i] <= 'Z')
+{
+result[i] = ((str[i] - 'A') + 13) % 26 + 'A';
+}
+else if (str[i] >= 'a' && str[i] <= 'z')
+{
+result[i] = ((str[i] - 'a') + 13) % 26 + 'a';
+}
+else
+{
+result[i] = str[i];
+}
+}
+result[i] = '\0';
+return (result);
+}
